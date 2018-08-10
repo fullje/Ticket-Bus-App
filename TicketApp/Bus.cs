eecdeeb1812ea077fc173c995939613ba6f15b09
fullje <fullje@gmail.com>
2018-08-10 15:22:12 +0200

    After few Hrs. Insert and Read from MySql Table

---
+++ TicketApp/Bus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+public class Bus
+{
+    public int id;
+    public int busNumber;
+    public int maxSeats;
+    public string desc;
+
+    public string setConnection = "SERVER=localhost;DATABASE=ticketdb;User ID=root;Password=;SSLmode=none";
+    MySqlConnection busDB = new MySqlConnection(setConnection);
+
+    //Read whole 'bus' db
+    public void readDB()
+    {
+
+    }
+
+    //Insert into 'bus' db
+    public void insertDB()
+    {
+
+    }
+
+
+}
--- TicketApp/MainWindow.xaml.cs
+++ TicketApp/MainWindow.xaml.cs
@@ -18,13 +18,67 @@ using System.Data;
 
 namespace TicketApp
 {
-    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+         
+        }
 
+        private void zatwierdzClick(object sender, RoutedEventArgs e)
+        {
+            Bus bus = new Bus();
+            bus.insertDB(Convert.ToInt32(idBox.Text), Convert.ToInt32(busNumber.Text), Convert.ToInt32(maxSeats.Text), desc.Text);
+            
+        }
+
+        private void loadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Bus bus = new Bus();
+            dataGrid1.ItemsSource = bus.readDB().DefaultView;
         }
     }
+
+    public class Bus
+    {
+        public static string setConnection = "SERVER=localhost;DATABASE=ticketdb;User ID=root;Password=;SSLmode=none";
+        MySqlConnection busDB = new MySqlConnection(setConnection);
+        
+
+        //Read whole 'bus' db
+        public DataTable readDB()
+        {
+            string Query = "SELECT * FROM bus";
+            MySqlCommand commandSelect = new MySqlCommand(Query, busDB);
+            MySqlDataAdapter adp = new MySqlDataAdapter(commandSelect);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+                        
+            busDB.Open();
+            
+            busDB.Close();
+
+            return dt;
+        }
+
+        //Insert into 'bus' db
+        public void insertDB(int id, int busNumber, int maxSeats, string des)
+        {
+            //MessageBox.Show(id + ", " + busNumber + ", " + maxSeats + ", " + des);
+            string Query = "INSERT INTO bus (id, busNumber, maxSeats, des) VALUES (" + id + "," + busNumber + "," + maxSeats + ",'" + des + "');";
+            MySqlCommand commandInsert = new MySqlCommand(Query, busDB);
+            MySqlDataReader reader;
+            busDB.Open();
+
+            reader = commandInsert.ExecuteReader();
+
+            reader.Close();
+            busDB.Close();
+        }
+            
+
+    }
+
+
 }
