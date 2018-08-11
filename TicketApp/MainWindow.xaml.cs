f76c7113ab764c9f711fb1ed64b8c5dd7d178e26
fullje <fullje@gmail.com>
2018-08-11 23:00:43 +0200

    Next day of learning

--- TicketApp/MainWindow.xaml.cs
+++ TicketApp/MainWindow.xaml.cs
@@ -23,14 +23,14 @@ namespace TicketApp
         public MainWindow()
         {
             InitializeComponent();
-         
+
         }
 
         private void zatwierdzClick(object sender, RoutedEventArgs e)
         {
             Bus bus = new Bus();
-            bus.insertDB(Convert.ToInt32(idBox.Text), Convert.ToInt32(busNumber.Text), Convert.ToInt32(maxSeats.Text), desc.Text);
-            
+            bus.insertDB(Convert.ToInt32(idBox.Text), Convert.ToInt32(busNumber.Text), Convert.ToInt32(maxSeats.Text), description.Text);
+
         }
 
         private void loadBtn_Click(object sender, RoutedEventArgs e)
@@ -38,47 +38,117 @@ namespace TicketApp
             Bus bus = new Bus();
             dataGrid1.ItemsSource = bus.readDB().DefaultView;
         }
-    }
 
-    public class Bus
-    {
-        public static string setConnection = "SERVER=localhost;DATABASE=ticketdb;User ID=root;Password=;SSLmode=none";
-        MySqlConnection busDB = new MySqlConnection(setConnection);
-        
-
-        //Read whole 'bus' db
-        public DataTable readDB()
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string Query = "SELECT * FROM bus";
-            MySqlCommand commandSelect = new MySqlCommand(Query, busDB);
-            MySqlDataAdapter adp = new MySqlDataAdapter(commandSelect);
-            DataTable dt = new DataTable();
-            adp.Fill(dt);
-                        
-            busDB.Open();
-            
-            busDB.Close();
 
-            return dt;
         }
 
-        //Insert into 'bus' db
-        public void insertDB(int id, int busNumber, int maxSeats, string des)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show(id + ", " + busNumber + ", " + maxSeats + ", " + des);
-            string Query = "INSERT INTO bus (id, busNumber, maxSeats, des) VALUES (" + id + "," + busNumber + "," + maxSeats + ",'" + des + "');";
-            MySqlCommand commandInsert = new MySqlCommand(Query, busDB);
-            MySqlDataReader reader;
-            busDB.Open();
-
-            reader = commandInsert.ExecuteReader();
-
-            reader.Close();
-            busDB.Close();
+            Bus bus = new Bus();
+            checkGrid.ItemsSource = bus.readDB().DefaultView;
         }
-            
+
+        private void rowSelected(object sender, SelectionChangedEventArgs e)
+        {
+            Ticket takeData = new Ticket();
+            string busNumber;
+            DataGrid dg = (DataGrid)sender;
+            DataRowView row_selected = dg.SelectedItem as DataRowView;
+            busNumber = row_selected["busNumber"].ToString();
+            displayGrid.ItemsSource = takeData.choosenDB(Convert.ToInt32(busNumber)).DefaultView;
+
+            Bus busData = new Bus();
+            DataGrid b_dg = (DataGrid)sender;
+            DataRowView rowBus_selected = b_dg.SelectedItem as DataRowView;
+
+            seatsNumberBox.Text = rowBus_selected["maxSeats"].ToString();
+            freeSeatsBox.Text = rowBus_selected["freeSeats"].ToString();
+            descriptionBox.Text = rowBus_selected["description"].ToString();
+
+
+        }
+
+        public class Ticket
+        {
+            static string setConnection = "SERVER=localhost;DATABASE=ticketdb;User ID=root;Password=;SSLmode=none";
+            MySqlConnection ticketDB = new MySqlConnection(setConnection);
+
+            //Read whole 'ticket' db
+            public DataTable readDB()
+            {
+                string Query = "SELECT * FROM ticket";
+                MySqlCommand commandSelect = new MySqlCommand(Query, ticketDB);
+                MySqlDataAdapter adp = new MySqlDataAdapter(commandSelect);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+
+                ticketDB.Open();
+
+                ticketDB.Close();
+
+                return dt;
+            }
+
+            public DataTable choosenDB(int bN)
+            {
+                string Query = "SELECT * FROM ticket WHERE (busNumber=" + bN + ");";
+
+                MySqlCommand commandSelect = new MySqlCommand(Query, ticketDB);
+                MySqlDataAdapter adp = new MySqlDataAdapter(commandSelect);
+
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+
+                ticketDB.Open();
+
+                ticketDB.Close();
+
+                return dt;
+            }
+        }
+
+        public class Bus
+        {
+            static string setConnection = "SERVER=localhost;DATABASE=ticketdb;User ID=root;Password=;SSLmode=none";
+            MySqlConnection busDB = new MySqlConnection(setConnection);
+
+            //Read whole 'bus' db
+            public DataTable readDB()
+            {
+                string Query = "SELECT * FROM bus";
+                MySqlCommand commandSelect = new MySqlCommand(Query, busDB);
+                MySqlDataAdapter adp = new MySqlDataAdapter(commandSelect);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+
+                busDB.Open();
+
+                busDB.Close();
+
+                return dt;
+            }
+
+            //Insert into 'bus' db
+            public void insertDB(int id, int busNumber, int maxSeats, string description)
+            {
+                //MessageBox.Show(id + ", " + busNumber + ", " + maxSeats + ", " + des);
+                int freeSeats = maxSeats;
+                string Query = "INSERT INTO bus (id, busNumber, maxSeats, freeSeats, description) VALUES (" + id + "," + busNumber + "," + maxSeats + "," + freeSeats + ",'" + description + "');";
+                MySqlCommand commandInsert = new MySqlCommand(Query, busDB);
+                MySqlDataReader reader;
+                busDB.Open();
+
+                reader = commandInsert.ExecuteReader();
+
+                reader.Close();
+                busDB.Close();
+            }
+
+
+        }
+
 
     }
-
-
 }
