e4c23cbf1ead26ae08e370091ee0b86a0b4d6445
fullje <fullje@gmail.com>
2018-08-13 13:35:33 +0200

    Small refact, improvements, bug fix, 2 bug next

--- TicketApp/MainWindow.xaml.cs
+++ TicketApp/MainWindow.xaml.cs
@@ -22,36 +22,58 @@ namespace TicketApp
     {
         public MainWindow()
         {
+            InitializeComponent();
+            loadAtStart();
+        }
+
+        private void loadAtStart()
+        {
             Bus bus = new Bus();
             Ticket ticket = new Ticket();
 
-            InitializeComponent();
+            description.IsEnabled = false;
+            maxSeats.IsEnabled = false;
+
+            applyBtn.IsEnabled = false;
+            zatwierdzBtn.IsEnabled = false;
+
 
             dataGrid1.ItemsSource = bus.readDB().DefaultView;
             checkGrid.ItemsSource = bus.readDB().DefaultView;
-            //TODO : FUNCITON TO LOAD ALL OF THE THINGS
-     
-
-
         }
 
+        //===================================================
+        // When dropDown = refresh view of comboBox content
+        //===================================================
         private void comboBox_1_DropDownOpened(object sender, EventArgs e)
         {
             Bus bus = new Bus();
 
-
             comboBox_1.ItemsSource = bus.readDB().DefaultView;
             comboBox_1.DisplayMemberPath = "description";
+
+            nameTxt.Text = "";
+            seatTxt.Text = "";
+            descriptionTxt.Text = "";
+
+            applyBtn.IsEnabled = false;
+            nameTxt.IsEnabled = false;
 
             comboSeat.Items.Clear();
 
         }
 
+        //===================================================
+        // Gets value from comboSeat (there are seats numbers)
+        // And put them to the text box
+        // And set the value of checkbox
+        //===================================================
         private void comboSeat_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (comboSeat.SelectedItem != null)
             {
                 seatTxt.Text = comboSeat.SelectedItem.ToString();
+                nameTxt.IsEnabled = true;
 
                 if (Convert.ToInt32(comboSeat.SelectedItem.ToString()) % 2 == 0)
                 {
@@ -62,149 +84,222 @@ namespace TicketApp
                     _hallway.IsChecked = true;
                     _window.IsChecked = false;
                 }
-                
-
             }
         }
 
+        //===================================================
+        // When dropDown then fill the comboBox with the 
+        // 'Description' values from DB 
+        // Also check the free Seats 
+        // 
+        //===================================================
         private void comboBox_1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
             Bus bus = new Bus();
             Ticket ticket = new Ticket();
-            int holder = 0;
-            int seatNbr = 0;
-            int busNbr = 0;
-            string freeSeat = null;
             ComboBox cmb = (ComboBox)sender;
-
-
             DataRowView drv = cmb.SelectedItem as DataRowView;
-            if(drv != null)
+
+            int holder = 0;
+            int busNbr = 0;
+            int seatNbr = 0;
+            string freeSeat = null;
+
+
+            if (drv != null)
             {
-                descriptionTxt.Text = drv["description"].ToString();
-                busNumberTxt.Text = drv["busNumber"].ToString();
+                descriptionTxt.Text = drv["description"].ToString(); // Set textbox with 'description'
+                busNumberTxt.Text = drv["busNumber"].ToString(); // Set textbox with 'busNumber'
 
-                holder = Convert.ToInt32(drv["maxSeats"].ToString()); //How many seats are set
-                busNbr = Convert.ToInt32(drv["busNumber"].ToString());
+                holder = Convert.ToInt32(drv["maxSeats"].ToString()); // Set holder (int) with 'maxSeats'
+                busNbr = Convert.ToInt32(drv["busNumber"].ToString()); // Set busNbr (int) with 'busNumber'
 
-                //MessageBox.Show("Bus number: " + busNbr.ToString());
-
+                // Creating array (only when user select from dropDown and only works for current bus
                 string[] seats = new string[holder];
 
-                DataTable dt = ticket.seats(busNbr); // Here has to be busNumber from selected ComboBox
+                DataTable dt = ticket.choosenDB(busNbr);
 
                 for (int y = 0; y < dt.Rows.Count; y++)
                 {
-                    //MessageBox.Show(dt.Rows[y]["seatNumber"].ToString());
-                    seatNbr = Convert.ToInt32(dt.Rows[y]["seatNumber"]); // Less in loop :v
-                    
-                    for (int i = 1; i < seats.Length+1; i++)
+                    seatNbr = Convert.ToInt32(dt.Rows[y]["seatNumber"]);
+
+                    for (int i = 1; i < seats.Length + 1; i++)
                     {
-                        //MessageBox.Show((i) + "========" + seatNbr.ToString());
                         if (i == seatNbr)
                         {
-                            seats[i-1] = seatNbr.ToString();
+                            seats[i - 1] = seatNbr.ToString();
                         }
                     }
                 }
-                
+
                 for (int z = 0; z < seats.Length; z++)
                 {
-                    //MessageBox.Show("Siedzenie numer: " + (z+1) + " jest " + seats[z]);
-
-                    if(seats[z] == null)
+                    if (seats[z] == null)
                     {
-                        //MessageBox.Show("Siedzenie numer: " + (z + 1) + " jest wolne");
                         freeSeat = (z + 1).ToString();
-
                         comboSeat.Items.Add(freeSeat);
                     }
                 }
             }
-
-
-            
-
-            //string[] seats = new string[]; // Here has to be array of maxSeats from bus
-
-            //for (int i = 0; i < dt.Rows.Count; i++)
-            //{
-                //MessageBox.Show(dt.Rows[i]["seatNumber"].ToString());
-                //comboSeat.Items.Add(dt.Rows[i]["seatNumber"].ToString());
-
-            //}
-
-
         }
 
+        //===================================================
+        // Apply button - Sends values to tickettDB
+        //===================================================
+        private void applyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Ticket ticket = new Ticket();
+            ticket.insertDB(nameTxt.Text.ToString(), Convert.ToInt32(busNumberTxt.Text.ToString()), Convert.ToInt32(seatTxt.Text.ToString()), _window.IsChecked.Value, _hallway.IsChecked.Value);
+
+            nameTxt.Text = "";
+            seatTxt.Text = "";
+
+            //++++++++++++++++++++
+            //Bug found - Should be reloaded not cleared! But it works...
+            //++++++++++++++++++++
+            comboSeat.Items.Clear(); 
+
+            applyBtn.IsEnabled = false;
+        }
+
+        //===================================================
+        // Sends values from textbox to busDB
+        // And reload view of dataGrid
+        //===================================================
         private void zatwierdzClick(object sender, RoutedEventArgs e)
         {
             Bus bus = new Bus();
             bus.insertDB(Convert.ToInt32(busNumber.Text), Convert.ToInt32(maxSeats.Text), description.Text);
             dataGrid1.ItemsSource = bus.readDB().DefaultView;
 
+            busNumber.Text = "";
+            maxSeats.Text = "";
+            description.Text = "";
+
         }
 
+        //===================================================
+        // Manual reload function
+        //===================================================
         private void loadBtn_Click(object sender, RoutedEventArgs e)
         {
             Bus bus = new Bus();
             dataGrid1.ItemsSource = bus.readDB().DefaultView;
         }
 
+        //===================================================
+        // Delete selected row from DB 
+        // Then reload view of dataGrid
+        //===================================================
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
             Bus bus = new Bus();
-
-            //string rowID = dataGrid1.SelectedItem.ToString();
             DataRowView rowView = dataGrid1.SelectedItem as DataRowView;
-            string busID = rowView.Row[1].ToString();
-
-            bus.deleteDB(Convert.ToInt32(busID));
-            dataGrid1.ItemsSource = bus.readDB().DefaultView;
-
-            //MessageBox.Show(rowID.ToString());
+            
+            if(rowView != null)
+            {
+                string busID = rowView.Row[1].ToString();
+                bus.deleteDB(Convert.ToInt32(busID));
+                dataGrid1.ItemsSource = bus.readDB().DefaultView;
+            }
         }
 
+        //===================================================
+        // Manual reload for dataGrid
+        //===================================================
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Bus bus = new Bus();
             checkGrid.ItemsSource = bus.readDB().DefaultView;
         }
 
-
+        //===================================================
+        // Display selected busNumber, how many seats are free
+        // and description
+        //===================================================
         private void rowSelected(object sender, SelectionChangedEventArgs e)
         {
             Ticket takeData = new Ticket();
-            string busNumber = null;
             DataGrid dg = (DataGrid)sender;
             DataRowView row_selected = dg.SelectedItem as DataRowView;
+            
+            string busNumber = null;
 
-            if(row_selected != null)
+            if (row_selected != null)
             {
                 busNumber = row_selected["busNumber"].ToString();
-                displayGrid.ItemsSource = takeData.choosenDB(Convert.ToInt32(busNumber)).DefaultView;
+                //++++++++++++++++++++
+                //Bug found - when clicked on empty datagrid
+                //++++++++++++++++++++
+                displayGrid.ItemsSource = takeData.choosenDB(Convert.ToInt32(busNumber)).DefaultView; 
 
                 Bus busData = new Bus();
                 DataGrid b_dg = (DataGrid)sender;
                 DataRowView rowBus_selected = b_dg.SelectedItem as DataRowView;
 
                 seatsNumberBox.Text = rowBus_selected["maxSeats"].ToString();
-                freeSeatsBox.Text = rowBus_selected["freeSeats"].ToString();
+                
                 descriptionBox.Text = rowBus_selected["description"].ToString();
-            }
-   
-            
+            }            
         }
 
+        
 
-        private void applyBtn_Click(object sender, RoutedEventArgs e)
+        //===================================================
+        // if name TextBox is empty
+        //===================================================
+        private void nameTxt_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Ticket ticket = new Ticket();
-            //MessageBox.Show(_window.IsChecked.Value.ToString(), _hallway.IsChecked.Value.ToString());
-            //(string name, int busNumber, int seatNumber, bool isFree, bool isWindow)
-            //MessageBox.Show(nameTxt.Text.ToString() + " " + busNumberTxt.Text.ToString() + " " + seatTxt.Text.ToString() + " " + _window.IsChecked.Value + " " +  _hallway.IsChecked.Value);
-            ticket.insertDB(nameTxt.Text.ToString(), Convert.ToInt32(busNumberTxt.Text.ToString()), Convert.ToInt32(seatTxt.Text.ToString()), _window.IsChecked.Value, _hallway.IsChecked.Value);
+            if (nameTxt.Text == "")
+            {
+                applyBtn.IsEnabled = false;
+                zatwierdzBtn.IsEnabled = false;
+            }
+            else
+            {
+                applyBtn.IsEnabled = true;
+            }
         }
+
+        private void busTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (busNumber.Text == "")
+            {
+                maxSeats.IsEnabled = false;
+                zatwierdzBtn.IsEnabled = false;
+            }
+            else
+            {
+                maxSeats.IsEnabled = true;
+            }
+        }
+
+        private void maxSeats_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (maxSeats.Text == "")
+            {
+                description.IsEnabled = false;
+                zatwierdzBtn.IsEnabled = false;
+            }
+            else
+            {
+                description.IsEnabled = true;
+            }
+        }
+
+        private void description_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (description.Text == "")
+            {
+                zatwierdzBtn.IsEnabled = false;
+            }
+            else
+            {
+                zatwierdzBtn.IsEnabled = true;
+            }
+        }
+        //=================================================
 
         public class Ticket
         {
@@ -227,6 +322,7 @@ namespace TicketApp
                 return dt;
             }
 
+            //Read only selected = busNumber
             public DataTable choosenDB(int bN)
             {
                 string Query = "SELECT * FROM ticket WHERE (busNumber=" + bN + ");";
@@ -244,23 +340,6 @@ namespace TicketApp
                 return dt;
             }
 
-
-            public DataTable seats(int busNumber)
-            {
-                string Query = "SELECT * FROM ticket WHERE busNumber=" + busNumber + ";";
-                
-                MySqlCommand commandSelect = new MySqlCommand(Query, ticketDB);
-
-                MySqlDataAdapter adp = new MySqlDataAdapter(commandSelect);
-                DataTable dt = new DataTable();
-                adp.Fill(dt);
-
-                ticketDB.Open();
-
-                ticketDB.Close();
-
-                return dt;
-            }
 
             public void insertDB(string name, int busNumber, int seatNumber, bool isFree, bool isWindow)
             {
@@ -334,5 +413,6 @@ namespace TicketApp
 
         }
 
+        
     }
 }
